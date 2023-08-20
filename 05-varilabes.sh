d0730c96807f441f59e9495759d701e0036df23f
sivaprasad <nalinshivaprasad@gmail.com>
2023-08-20 11:13:19 +0530

    05 ADDING PASSWORD

--- 05-varilabes.sh
+++ 05-varilabes.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-echo " Please enger your username "
+echo " Please enter your username "
 read USERNAME #THIS I THE VARIABLE WHRE THE VALHE IF THE ONE USER ENTERERED
-
 echo " Username entered is :$USERNAME"
+echo " Please enter your password"
+read PASSWORD
+echo " Username entered is :$PASSWORD"
