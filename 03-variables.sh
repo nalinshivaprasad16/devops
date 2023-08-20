59c5fc367e388614f9257a03d8ac2fba1c88bb31
sivaprasad <nalinshivaprasad@gmail.com>
2023-08-20 10:12:06 +0530

    03 vairalbes

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!bin/bash
+echo "Hello world, I am learning Devops with AWS"
+PERSON1=$1
+PERSON2=$2
+echo "$PERSON1: goodmorning $PERSON2"
+echo "$PERSON2: goodmorning $PERSON1"
+echo "$PERSON1: how are you $PERSON2"
+echo "$PERSON2: i am fine how are you $PERSON1"
+
