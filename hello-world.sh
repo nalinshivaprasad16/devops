aad3b9f483f1b78d2cc27141e934ed1b90dd36b6
sivaprasad <nalinshivaprasad@gmail.com>
2023-08-20 09:36:51 +0530

    new lines in scripting

--- hello-world.sh
+++ hello-world.sh
@@ -1,2 +1,6 @@
 #!bin/bash
 echo "Hello world, I am learning Devops with AWS"
+echo "Nalin: goodmorning vedanta"
+echo "vedanta: goodmorning dad"
+echo "Nalin: how are you "
+cho "vedanta: i am fine how are you dad"
