1ef2fa9ed109ca66013f44e7d917273904822d9b
sivaprasad <nalinshivaprasad@gmail.com>
2023-08-20 11:34:55 +0530

    06 Array

--- 06-array.sh
+++ 06-array.sh
@@ -2,4 +2,4 @@
 #This is called array , insted of single value it cam ehold multiple values
 PERSONS=("NALIN" "SHIVA" "PRASAD")
 #inside the array we always refer values with the indeint and indent start from 0
-echo "First Person: $(PERSOPNS[0])"
+echo "First Person: $(PERSON[0])"
